d07ba13ce800ac67c1de4c6ab7adf94c3253a389
Mattias Eriksson <thematerik@gmail.com>
2015-06-05 15:28:14 -0700

    BUMPED the version number to 1.0.0

--- index.js
+++ index.js
@@ -1,1 +1,159 @@
-module.exports = require('./lib/');
+var _ = require('underscore');
+var _s = require('underscore.string');
+var crypto = require('crypto');
+
+var CRYPTO_BASE_64 = 'base64';
+var CRYPTO_HEX = 'hex';
+var CRYPTO_SHA1 = 'sha1';
+var PATH_ID = ':id';
+var REGEX_EMAIL = /[\w\d_]+@\w+\.\w+[\w\.]*/;
+var REGEX_MONTH = /20\d{2}\-(0(?=[1-9])|1(?=[0-2]{1}))\d{1}/;
+var REGEX_URL = /http[s]{0,1}:\/\/(www\.){0,1}\w+\.{1}\w+[\w\/#\?&]*/;
+var REGEX_NBR_WHOLE = /^\d+$/;
+var REGEX_NBR_DOT = /\d+\./;
+var STR_DOT = '.';
+var STR_DOT_ZERO = '.0';
+var STR_EMPTY = '';
+var STR_ZERO = '0';
+var STR_ZERO_DOT = '0.';
+
+module.exports = {
+
+    base64encode: function(str) {
+        return new Buffer(str).toString(CRYPTO_BASE_64);
+    },
+
+    camelCaseStr: function(str) {
+        str = str.toLowerCase();
+        return _s.camelize(str);
+    },
+
+    dotGet: function(obj, key) {
+        var ret = obj;
+        var keySplit = key.split(STR_DOT);
+        for (var i in keySplit) {
+            var k = keySplit[i];
+            if (ret) {
+                ret = ret[k];
+            }
+        }
+        return ret;
+    },
+
+    dotSet: function(obj, key, val) {
+        var ret = obj;
+        var keySplit = key.split(STR_DOT);
+        var lastKey = keySplit.pop();
+        for (var i in keySplit) {
+            var k = keySplit[i];
+            ret = ret[k];
+        }
+        if (ret) {
+            ret[lastKey] = val;
+        }
+    },
+
+    forEachAndDone: function(objs, iter, done) {
+        if (objs && objs.length) {
+            var obj = objs.pop();
+            iter(obj, function() {
+                module.exports.forEachAndDone(objs, iter, done);
+            });
+        } else {
+            done();
+        }
+    },
+
+    getPaths: function(dict) {
+        var self = module.exports;
+        var paths = [];
+        for (var key in dict) {
+            var val = dict[key];
+            if (_.isObject(val)) {
+                var nestedPaths = self.getPaths(val);
+                if (nestedPaths.length) {
+                    for (var i in nestedPaths) {
+                        var np = nestedPaths[i];
+                        if (np.match(REGEX_NBR_WHOLE)) {
+                            np = 0;
+                        } else {
+                            np = np.replace(REGEX_NBR_DOT, STR_ZERO_DOT);
+                        }
+                        paths.push(key + STR_DOT + np);
+                    }
+                } else if (_.isArray(val)) {
+                    paths.push(key + STR_DOT_ZERO);
+                }
+            } else {
+                paths.push(key);
+            }
+        }
+        return _.uniq(paths);
+    },
+
+    getReqPath: function(req) {
+        var path = req.url;
+        if (!path) {
+            path = req.path;
+        }
+        if (req.params && req.params.id) {
+            path = path.replace(PATH_ID, req.params.id);
+        }
+        return path;
+    },
+
+    httpMethod: {
+        DELETE: 'DELETE',
+        GET: 'GET',
+        POST: 'POST',
+        PUT: 'PUT',
+    },
+
+    isJSONParseable: function(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    },
+
+    isValidEmail: function(email) {
+        if (!email) {
+            return false;
+        }
+        return !_.isNull(email.match(REGEX_EMAIL));
+    },
+
+    isValidMonth: function(month) {
+        if (!month) {
+            return false;
+        }
+        return !_.isNull(month.match(REGEX_MONTH));
+    },
+
+    isValidURL: function(url) {
+        if (!url) {
+            return false;
+        }
+        return !_.isNull(url.match(REGEX_URL));
+    },
+
+    makeSalt: function() {
+        return Math.round((new Date().valueOf() * Math.random())) + STR_EMPTY;
+    },
+
+    prependZeros: function(str, length) {
+        str = str.toString();
+        while (str.length < length) {
+            str = STR_ZERO + str;
+        }
+        return str;
+    },
+
+    sha1encrypt: function(salt, string) {
+        var hmac = crypto.createHmac(CRYPTO_SHA1, salt);
+        return hmac.update(string).digest(CRYPTO_HEX);
+    },
+
+};
--- test.js
+++ test.js
@@ -1,111 +1,118 @@
-var utils = require('./lib/index.js')
+var assert = require('assert');
+var utils = require('./index');
 
+describe('utils', function() {
 
-exports.testCamelCaseStr = function(test) {
-    var m = utils.camelCaseStr
-    test.equal(m('hej du'), 'hejDu')
-    test.equal(m('model name'), 'modelName')
-    test.equal(m('MODEL'), 'model')
-    test.equal(m('MODEL NAME'), 'modelName')
-    test.equal(m('hej pa dig din dumbom'), 'hejPaDigDinDumbom')
-    test.equal(m('hej_DA'), 'hejDa')
-    test.done()
-}
+    it('base64encode', function(callback) {
+        var m = utils.base64encode;
+        assert.equal(m('jlpicard:Changeme1'), 'amxwaWNhcmQ6Q2hhbmdlbWUx');
+        assert.equal(m('materik:asdf'), 'bWF0ZXJpazphc2Rm');
+        callback();
+    });
 
-exports.testIsValidURL = function(test) {
-    test.ok(!utils.isValidURL(null))
-    test.ok(utils.isValidURL('http://materik.com'))
-    test.ok(utils.isValidURL('https://materik.com'))
-    test.ok(utils.isValidURL('http://www.materik.com'))
-    test.ok(utils.isValidURL('http://www.materik.com/hej'))
-    test.ok(utils.isValidURL('http://www.materik.com/#/projects'))
-    test.ok(utils.isValidURL('http://www.materik.com?a=b&c=d'))
-    test.ok(!utils.isValidURL('materik.com'))
-    test.ok(!utils.isValidURL('www.materik.com'))
-    test.ok(!utils.isValidURL('httpx://materik.com'))
-    test.ok(!utils.isValidURL('http:/materik.com'))
-    test.ok(!utils.isValidURL('http://www'))
-    test.ok(!utils.isValidURL('http://www.'))
-    test.done()
-}
+    it('camelCaseStr', function(callback) {
+        var m = utils.camelCaseStr;
+        assert.equal(m('hej du'), 'hejDu');
+        assert.equal(m('model name'), 'modelName');
+        assert.equal(m('MODEL'), 'model');
+        assert.equal(m('MODEL NAME'), 'modelName');
+        assert.equal(m('hej pa dig din dumbom'), 'hejPaDigDinDumbom');
+        assert.equal(m('hej_DA'), 'hejDa');
+        callback();
+    });
 
-exports.testIsValidEmail = function(test) {
-    test.ok(!utils.isValidEmail(null))
-    test.ok(utils.isValidEmail('materik@me.com'))
-    test.ok(utils.isValidEmail('materik@student.chalmers.se'))
-    test.ok(utils.isValidEmail('erixs_21@hotmail.com'))
-    test.ok(utils.isValidEmail('a@b.c'))
-    test.ok(!utils.isValidEmail('a@b'))
-    test.ok(!utils.isValidEmail('@b'))
-    test.ok(!utils.isValidEmail('ab'))
-    test.done()
-}
+    it('getPaths', function(callback) {
+        var m = utils.getPaths;
+        var x = {'x': 1, 'y': 2};
+        assert.deepEqual(m(x), ['x', 'y']);
+        x = {'x': 1, 'y': 2, 'z': 3};
+        assert.deepEqual(m(x), ['x', 'y', 'z']);
+        x = {'x': 1, 'y': {'x': 2, 'z': 3}};
+        assert.deepEqual(m(x), ['x', 'y.x', 'y.z']);
+        x = {'x': 1, 'y': {'x': {'z': 3}}};
+        assert.deepEqual(m(x), ['x', 'y.x.z']);
+        x = {'x': 1, 'y': {}};
+        assert.deepEqual(m(x), ['x']);
+        x = {'x': 1, 'y': [{'z': 1}]};
+        assert.deepEqual(m(x), ['x', 'y.0.z']);
+        x = {'x': 1, 'y': [{'z': 1}, {'z': 2}]};
+        assert.deepEqual(m(x), ['x', 'y.0.z']);
+        x = {'x': 1, 'y': [{'z': 1}, {'o': 2}]};
+        assert.deepEqual(m(x), ['x', 'y.0.z', 'y.0.o']);
+        x = {'x': 1, 'y': []};
+        assert.deepEqual(m(x), ['x', 'y.0']);
+        x = {'x': 1, 'y': ['a', 'b', 'c']};
+        assert.deepEqual(m(x), ['x', 'y.0']);
+        callback();
+    });
 
-exports.testPrependZeros = function(test) {
-    test.equal(utils.prependZeros(0, 1), '0')
-    test.equal(utils.prependZeros(0, 2), '00')
-    test.equal(utils.prependZeros(1+1, 2), '02')
-    test.equal(utils.prependZeros('1', 1), '1')
-    test.equal(utils.prependZeros('1', 2), '01')
-    test.equal(utils.prependZeros('123', 2), '123')
-    test.equal(utils.prependZeros('123', 3), '123')
-    test.equal(utils.prependZeros('123', 4), '0123')
-    test.equal(utils.prependZeros('123', 5), '00123')
-    test.done()
-}
-
-exports.testBase64encode = function(test) {
-    var m = utils.base64encode
-    test.equal(m('jlpicard:Changeme1'), 'amxwaWNhcmQ6Q2hhbmdlbWUx')
-    test.equal(m('materik:asdf'), 'bWF0ZXJpazphc2Rm')
-    test.done()
-}
-
-exports.testIsValidMonth = function(test) {
-    var m = utils.isValidMonth
-    for (i = 1; i < 13; i++) {
-        if (i < 10) {
-            test.ok(m('2013-0' + i))
-        } else {
-            test.ok(m('2013-' + i))
+    it('isValidMonth', function(callback) {
+        var m = utils.isValidMonth;
+        for (i = 1; i < 13; i++) {
+            if (i < 10) {
+                assert(m('2013-0' + i));
+            } else {
+                assert(m('2013-' + i));
+            }
         }
-    }
-    test.ok(m('2013-03'))
-    test.ok(m('2013-11'))
-    test.ok(m('2015-12'))
-    test.ok(!m('0'))
-    test.ok(!m('2-00'))
-    test.ok(!m('2000-0'))
-    test.ok(!m('2015-00'))
-    test.ok(!m('2015-13'))
-    test.ok(!m('2015-17'))
-    test.ok(!m('2115-12'))
-    test.ok(!m('2115-22'))
-    test.ok(!m('3333-01'))
-    test.done()
-}
+        assert(m('2013-03'));
+        assert(m('2013-11'));
+        assert(m('2015-12'));
+        assert(!m('0'));
+        assert(!m('2-00'));
+        assert(!m('2000-0'));
+        assert(!m('2015-00'));
+        assert(!m('2015-13'));
+        assert(!m('2015-17'));
+        assert(!m('2115-12'));
+        assert(!m('2115-22'));
+        assert(!m('3333-01'));
+        callback();
+    });
 
-exports.testGetPaths = function(test) {
-    var m = utils.getPaths
-    var x = {'x': 1, 'y': 2}
-    test.deepEqual(m(x), ['x', 'y'])
-    var x = {'x': 1, 'y': 2, 'z': 3}
-    test.deepEqual(m(x), ['x', 'y', 'z'])
-    var x = {'x': 1, 'y': {'x': 2, 'z': 3}}
-    test.deepEqual(m(x), ['x', 'y.x', 'y.z'])
-    var x = {'x': 1, 'y': {'x': {'z': 3}}}
-    test.deepEqual(m(x), ['x', 'y.x.z'])
-    var x = {'x': 1, 'y': {}}
-    test.deepEqual(m(x), ['x'])
-    var x = {'x': 1, 'y': [{'z': 1}]}
-    test.deepEqual(m(x), ['x', 'y.0.z'])
-    var x = {'x': 1, 'y': [{'z': 1}, {'z': 2}]}
-    test.deepEqual(m(x), ['x', 'y.0.z'])
-    var x = {'x': 1, 'y': [{'z': 1}, {'o': 2}]}
-    test.deepEqual(m(x), ['x', 'y.0.z', 'y.0.o'])
-    var x = {'x': 1, 'y': []}
-    test.deepEqual(m(x), ['x', 'y.0'])
-    var x = {'x': 1, 'y': ['a', 'b', 'c']}
-    test.deepEqual(m(x), ['x', 'y.0'])
-    test.done()
-}
+    it('isValidEmail', function(callback) {
+        var m = utils.isValidEmail;
+        assert(!m(null));
+        assert(m('materik@me.com'));
+        assert(m('materik@student.chalmers.se'));
+        assert(m('erixs_21@hotmail.com'));
+        assert(m('a@b.c'));
+        assert(!m('a@b'));
+        assert(!m('@b'));
+        assert(!m('ab'));
+        callback();
+    });
+
+    it('isValidURL', function(callback) {
+        var m = utils.isValidURL;
+        assert(!m(null));
+        assert(m('http://materik.com'));
+        assert(m('https://materik.com'));
+        assert(m('http://www.materik.com'));
+        assert(m('http://www.materik.com/hej'));
+        assert(m('http://www.materik.com/#/projects'));
+        assert(m('http://www.materik.com?a=b&c=d'));
+        assert(!m('materik.com'));
+        assert(!m('www.materik.com'));
+        assert(!m('httpx://materik.com'));
+        assert(!m('http:/materik.com'));
+        assert(!m('http://www'));
+        assert(!m('http://www.'));
+        callback();
+    });
+
+    it('prependZeros', function(callback) {
+        var m = utils.prependZeros;
+        assert.equal(m(0, 1), '0');
+        assert.equal(m(0, 2), '00');
+        assert.equal(m(1 + 1, 2), '02');
+        assert.equal(m('1', 1), '1');
+        assert.equal(m('1', 2), '01');
+        assert.equal(m('123', 2), '123');
+        assert.equal(m('123', 3), '123');
+        assert.equal(m('123', 4), '0123');
+        assert.equal(m('123', 5), '00123');
+        callback();
+    });
+
+});
